5811e43f3c5c7d1d5baeeedf55162fb1f34b31d9
Darius Rapalis <info@rdarius.lt>
2025-03-10 21:40:07 +0200

    #2

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
 	fmt.Println("Hello, World!")
 
+}
+
+func cleanInput(text string) []string {
+	// trim input string
+	text = strings.TrimSpace(text)
+	// convert to lowercase
+	text = strings.ToLower(text)
+	// replace all double spaces into single space
+	text = strings.Replace(text, "  ", " ", -1)
+	// split by space
+	separated := strings.Split(text, " ")
+	return separated
 }
