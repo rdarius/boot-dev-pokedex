c6e41f607a3b35349079876ee340eef6bd932fc2
Darius Rapalis <info@rdarius.lt>
2025-03-10 21:47:13 +0200

    #3

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	quit := false
+
+	for !quit {
+
+		// read user input
+		input := bufio.NewScanner(os.Stdin)
+		fmt.Print("Pokedex > ")
+		for input.Scan() {
+			word := input.Text()
+			cleanedWord := cleanInput(word)
+			fmt.Printf("Your command was: %s\n", cleanedWord[0])
+
+			if cleanedWord[0] == "quit" {
+				quit = true
+			}
+			break
+		}
+
+	}
 
 }
 
